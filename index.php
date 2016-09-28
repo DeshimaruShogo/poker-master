ed3b3ec2032cb9a1e1f4a88106a4299dc5b2214f
DeshimaruShogo <s_dsmr@yahoo.co.jp>
2016-09-28 21:57:08 +0900

    use filter_input

--- index.php
+++ index.php
@@ -1,44 +1,45 @@
 <?php
-    require 'process.php'; // 裏側処理用ページ
+require 'process.php'; // 裏側処理用ページ
+$filter = filter_input(INPUT_POST, 'flg');
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Poker Game</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Poker Game</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
 
-<div class="table">
-    <?php if($_POST) : ?>
-        <?php foreach ($enemy_show_hand as $card) : // 手札の表示（相手）?>
-            <img src="./image_trump/gif/<?php echo $card ?>.gif">
-        <?php endforeach; ?>
+        <div class="table">
+            <?php if ($filter) : ?>
+                <?php foreach ($enemy_show_hand as $card) : // 手札の表示（相手）?>
+                    <img src="./image_trump/gif/<?php echo $card ?>.gif">
+                <?php endforeach; ?>
 
-        <p>相手の役は<span><?php echo $enemy_yaku; ?></span>です。</p>
-    <?php endif; ?>
-</div>
+                <p>相手の役は<span><?php echo $enemy_yaku; ?></span>です。</p>
+            <?php endif; ?>
+        </div>
 
-<div class="stock">
-    <form method='post'>
-        <h2>↓カードを引くと↓</h2>
-        <input type="submit" value="" name="flg" id="stockImage">
-        <h2>↑プレイできます↑</h2>
-    </form>
-</div>
+        <div class="stock">
+            <form method='post' name="flg">
+                <h2>↓カードを引くと↓</h2>
+                <input type="submit" value="" id="stockImage">
+                <h2>↑プレイできます↑</h2>
+            </form>
+        </div>
 
-<div class="table">
-    <?php if($_POST) : ?>
-        <?php foreach ($player_show_hand as $card) : // 手札の表示（あなた）?>
-            <img src="./image_trump/gif/<?php echo $card ?>.gif">
-        <?php endforeach; ?>
+        <div class="table">
+            <?php if ($_POST) : ?>
+                <?php foreach ($player_show_hand as $card) : // 手札の表示（あなた）?>
+                    <img src="./image_trump/gif/<?php echo $card ?>.gif">
+                <?php endforeach; ?>
 
-        <p>あなたの役は<span><?php echo $player_yaku; ?></span>です。</p>
-    <?php endif; ?>
-</div>
+                <p>あなたの役は<span><?php echo $player_yaku; ?></span>です。</p>
+            <?php endif; ?>
+        </div>
 
-</body>
+    </body>
 </html>
